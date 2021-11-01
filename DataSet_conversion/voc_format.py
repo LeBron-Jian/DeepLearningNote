24f99ca600ebc35cab7b079ed82e3a2a5df4c98f
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2021-11-01 10:14:34 +0800

    Create voc_format.py

---
+++ DataSet_conversion/voc_format.py
@@ -0,0 +1,40 @@
+headstr = """\
+<annotation>
+    <folder>VOC</folder>
+    <filename>%s</filename>
+    <source>
+        <database>My Database</database>
+        <annotation>COCO</annotation>
+        <image>flickr</image>
+        <flickrid>NULL</flickrid>
+    </source>
+    <owner>
+        <flickrid>NULL</flickrid>
+        <name>company</name>
+    </owner>
+    <size>
+        <width>%d</width>
+        <height>%d</height>
+        <depth>%d</depth>
+    </size>
+    <segmented>0</segmented>
+"""
+
+objstr = """\
+    <object>
+        <name>%s</name>
+        <pose>Unspecified</pose>
+        <truncated>0</truncated>
+        <difficult>0</difficult>
+        <bndbox>
+            <xmin>%d</xmin>
+            <ymin>%d</ymin>
+            <xmax>%d</xmax>
+            <ymax>%d</ymax>
+        </bndbox>
+    </object>
+"""
+ 
+tailstr = '''\
+</annotation>
+'''
